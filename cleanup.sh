0bc9bb22472d4bc278b596da4b49b304d350c8c9
Niccolo' Zanotti <niccolo.zanotti@icloud.com>
2024-11-30 20:36:46 +0100

    edit(cleanup): cleanup also higher-order products of .

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,7 @@
 echo "Cleaning up old auxiliary files..."
 
 # Remove matching files in the current directory
-rm -f *.{aux,bbl,bcf,blg,log,lot,out,toc,run.xml,synctex.gz,bak0,bak1,bak2,bak3,bak4}
+rm -f *.{aux,bbl,bcf,blg,log,lot,out,toc,run.xml,synctex.gz,bak0,bak1,bak2,bak3,bak4,bak5,bak6,bak7,bak8,bak9,bak10}
 
 # Remove matching files in subdirectories up to a depth of 2
 find . -maxdepth 2 -type f \( \
@@ -22,7 +22,13 @@ find . -maxdepth 2 -type f \( \
     -name "*.bak1" -o \
     -name "*.bak2" -o \
     -name "*.bak3" -o \
-    -name "*.bak4" \
+    -name "*.bak4" -o \
+    -name "*.bak5" -o \
+    -name "*.bak6" -o \
+    -name "*.bak7" -o \
+    -name "*.bak8" -o \
+    -name "*.bak9" -o \
+    -name "*.bak10" \
 \) -exec rm -f {} +
 
 echo "Cleanup complete."
